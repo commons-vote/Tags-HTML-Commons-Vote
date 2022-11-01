173064ef95f8c8e84ec894b0485269841de020ef
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-11-01 01:44:28 +0100

    Add link to Wikidata item

--- Vote/Competition.pm
+++ Vote/Competition.pm
@@ -185,8 +185,18 @@ sub _process {
 	tags_dl_item($self, 'organizer', $competition->organizer);
 	tags_dl_item($self, 'date_image_loaded',
 		dt_format($self, $competition->dt_images_loaded));
-	tags_dl_item($self, 'wd_qid', $competition->wd_qid);
 	$self->{'tags'}->put(
+		['b', 'dt'],
+		['d', text($self, 'wd_qid')],
+		['e', 'dt'],
+
+		['b', 'dd'],
+		['b', 'a'],
+		['a', 'href', 'https://www.wikidata.org/wiki/'.$competition->wd_qid],
+		['d', $competition->wd_qid],
+		['e', 'a'],
+		['e', 'dd'],
+
 		['b', 'dt'],
 		['d', text($self, 'voting_types')],
 		['e', 'dt'],
